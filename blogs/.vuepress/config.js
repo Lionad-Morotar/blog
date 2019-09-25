03c04934d555ede37b06b3ad408f22bd2f2cfee5
lionad <tangnad@qq.com>
2019-09-26 01:16:15 +0800

    blogs: 博客更名；Readme.md；

--- blogs/.vuepress/config.js
+++ blogs/.vuepress/config.js
@@ -8,8 +8,8 @@ module.exports = {
 
   /** page config */
 
-  title: "MGear Blog",
-  description: "Lionad 的个人博客, 心流历程以及其它一些好玩的东西",
+  title: "Lionad-Guirotar Blogs",
+  description: "Lionad Guirotar 的个人博客, 心流历程以及其它一些好玩的东西",
   head: [
     ["link", { rel: 'dns-prefetch', href: '//utteranc.es' }],
     ["link", { rel: 'shortcut icon', href: '/favicon.ico' }],
