87b42e44c4ea54f54cdb0df7b987d378ca069e0e
lionad <tangnad@qq.com>
2019-10-18 02:00:32 +0800

    feat: 测试给最新的一篇博客应用 subfonts

---
+++ blogs/.vuepress/components/Subfonts-wanwulianjie.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="placeholder"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus">
+@import './subfonts/fonts-0a60461fa1.css';
+
+.placeholder {
+  content: 'import fonts';
+}
+</style>
