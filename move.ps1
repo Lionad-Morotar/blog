5ee49210eebe2eb353d4b9f9f5d1631a0480379a
lionad <tangnad@qq.com>
2019-10-25 02:17:31 +0800

    subfont

--- move.ps1
+++ move.ps1
@@ -1,5 +1,5 @@
 
-                ren tmp/subfont 672419903
-                mv tmp/672419903 blogs/.vuepress/components/subfont
-                mv tmp/Subfonts-672419903.vue blogs/.vuepress/components
+                ren tmp/subfont 1429091841
+                mv tmp/1429091841 blogs/.vuepress/components/subfont
+                mv tmp/Subfonts-1429091841.vue blogs/.vuepress/components
                 
