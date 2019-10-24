414f47aaa9720fa071debde8afe7b9806db9c20f
lionad <tangnad@qq.com>
2019-10-25 02:19:00 +0800

    subfont

---
+++ blogs/.vuepress/components/Subfonts-1728146593.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="placeholder"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+@import './subfont/1728146593/index.css';
+
+.placeholder {
+  content: 'import fonts';
+}
+</style>
--- move.ps1
+++ move.ps1
@@ -1,5 +1,5 @@
 
-                ren tmp/subfont 1821334017
-                mv tmp/1821334017 blogs/.vuepress/components/subfont
-                mv tmp/Subfonts-1821334017.vue blogs/.vuepress/components
+                ren tmp/subfont 1728146593
+                mv tmp/1728146593 blogs/.vuepress/components/subfont
+                mv tmp/Subfonts-1728146593.vue blogs/.vuepress/components
                 
