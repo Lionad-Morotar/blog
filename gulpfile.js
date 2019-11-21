f521f42cafdb4d597d8b010d472844f0d93feee5
lionad <tangnad@qq.com>
2019-11-22 03:06:50 +0800

    feat: sidebar 的字体使用默认字体

--- gulpfile.js
+++ gulpfile.js
@@ -37,47 +37,6 @@ const fontHTML = content =>
   `
 <html>
 <body>
-  <!-- sidebar -->
-  <div>
-    {
-      title: '心流 / Heart Flows',
-      collapsable: false,
-      children: [
-        'flow/万物联结与幸福感',
-        'flow/偷取时间',
-        'flow/隐秘',
-        'flow/追寻自由与满怀希望',
-        'flow/表达和孤独',
-        'flow/文字与情绪',
-        'flow/使用英文点号',
-        'flow/一封沉默',
-        'flow/我的腼腆',
-      ]
-    },
-    {
-      title: '博客 / Blogs',
-      collapsable: false,
-      children: [
-        '006-聊聊聊此次离职毕业及其它杂乱的事情',
-        '005-你本可以少写些if-else',
-        '004-150行代码带你实现小程序中的数据侦听',
-        '003-从一个越写越慢的编辑器中聊聊优化思路',
-        '002-探索Scoped-CSS实现原理',
-        '001-听说你还在手写懒加载'
-      ]
-    },
-    {
-      title: '单曲 / Songs',
-      collapsable: false,
-      children: [
-        '163/wuti',
-        '163/wings-you-are-the-hero',
-        '163/奇迹之山',
-        '163/wu-wei',
-        '163/EVA'
-      ]
-    }
-  </div>
   <!-- normal -->
   <div>
     abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890 -=[];',./\\|<>?:"{}_+"'~\`!@#$%^&*()|
