fd44c547ed0521470a35f3fdc74a41f079639405
lionad <tangnad@qq.com>
2019-12-30 01:27:20 +0800

    fixlast: 字体引入

--- blogs/.vuepress/components/Font/Subfonts-1855688673.vue
+++ blogs/.vuepress/components/Font/Subfonts-1855688673.vue
@@ -1,10 +1,13 @@
-
 <template>
   <div class="placeholder"></div>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    document.body.classList.value = 'font-1855688673'
+  }
+}
 </script>
 
 <style>
