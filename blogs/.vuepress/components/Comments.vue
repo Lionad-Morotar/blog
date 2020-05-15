559eb9c9c7f3f4d4108dd6d9cbfa85392bb93f0f
Lionad <tangnad@qq.com>
2020-05-15 18:46:08 +0800

    feat

--- blogs/.vuepress/components/Comments.vue
+++ blogs/.vuepress/components/Comments.vue
@@ -15,6 +15,7 @@ export default {
             const utterances = document.createElement('script')
             utterances.type = 'text/javascript'
             utterances.async = true
+            utterances.defer = true
             utterances.setAttribute('issue-term', 'title')
             utterances.setAttribute('label', '💬')
             utterances.setAttribute('theme', 'github-light')
@@ -28,6 +29,7 @@ export default {
             const crisp = document.createElement('script')
             crisp.type = 'text/javascript'
             crisp.async = true
+            crisp.defer = true
             crisp.crossorigin = 'anonymous'
             crisp.innerHTML = `/** CRISP */ window.$crisp = []; window.CRISP_WEBSITE_ID = '645341f5-8128-4961-a5b8-7b7dfb057cdd'; ;(function() { d = document; s = d.createElement('script'); s.src = 'https://client.crisp.chat/l.js'; s.async = 1; d.getElementsByTagName('head')[0].appendChild(s); })();`
             window.document.body.appendChild(crisp)
