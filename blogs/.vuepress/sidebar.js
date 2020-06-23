a23c351aa889921efc8698b07138fa3415688de8
Lionad <tangnad@qq.com>
2020-06-24 01:03:54 +0800

    rename

--- blogs/.vuepress/sidebar.js
+++ blogs/.vuepress/sidebar.js
@@ -59,7 +59,7 @@ const sidebarConfigs = {
             collapsable: false,
             open: true,
             children: [
-                'zebra-puzzle',
+                '斑马难题',
                 'CSSAA',
                 '你可以用纯CSS判断鼠标进入的方向吗',
                 '9种CSS图案解析',
