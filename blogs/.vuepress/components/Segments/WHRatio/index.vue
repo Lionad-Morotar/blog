0cf7065a37ae1098f296981d80829f8b08e9f247
Lionad <tangnad@qq.com>
2020-06-24 11:12:28 +0800

    feat: ...

--- blogs/.vuepress/components/Segments/WHRatio/index.vue
+++ blogs/.vuepress/components/Segments/WHRatio/index.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="wh-container" :style="{ width: this.w }">
-    <slot name="default" />
-    <div class="padding-box" :style="{ paddingBottom: this.h }" />
-  </div>
+    <div class="wh-container" :style="{ width: this.w }">
+        <slot name="default" />
+        <div class="padding-box" :style="{ paddingTop: this.h }" />
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'WHRatio',
-  props: {
-    w: {
-      type: String,
-      default: '100%'
-    },
-    h: {
-      type: String,
-      default: '100%'
+    name: 'WHRatio',
+    props: {
+        w: {
+            type: String,
+            default: '100%'
+        },
+        h: {
+            type: String,
+            default: '100%'
+        }
     }
-  }
 }
 </script>
 
 <style lang="stylus">
 .wh-container {
-  display flex
+  display: flex;
 
   .padding-box {
-    width 0
-    height 0
+    width: 0;
+    height: 0;
   }
 }
 </style>
