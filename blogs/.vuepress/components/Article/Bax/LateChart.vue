142f013e7063d6f83b9f5e1fbd961077a5b32cd3
Lionad <tangnad@qq.com>
2021-04-09 14:21:32 +0800

    feat: 本周迟到基金概览

--- blogs/.vuepress/components/Article/Bax/LateChart.vue
+++ blogs/.vuepress/components/Article/Bax/LateChart.vue
@@ -8,13 +8,10 @@
             <th scope="row">
               <template v-if="recIDX % 7 < 5">
                 {{ rec.month }} / {{ rec.day }}
+                <div v-if="rec.weather === 'sun'" class="sun" />
+                <div v-else-if="rec.weather === 'cloud'" class="cloud" />
+                <div v-else class="rain"><div class="dot" /></div>
               </template>
-              <template v-else>
-                * / *
-              </template>
-              <div v-if="rec.weather === 'sun'" class="sun" />
-              <div v-else-if="rec.weather === 'cloud'" class="cloud" />
-              <div v-else class="rain"><div class="dot" /></div>
             </th>
             <td :style="`--size: calc(${15 + (rec.record||[]).length * 15} / 120)`">
               <span class="data">
@@ -32,49 +29,25 @@
         </tbody>
       </table>
 
-      <table id="chart-money-count" class="charts-css line show-data-axes">
+      <table id="chart-money-count-day" class="charts-css line show-data-axes" style="--color: #ffe583;">
         <tbody>
-          <tr>
-            <td style="--start: 0.0; --size: 0.0"> <span class="data"> ￥0 </span> </td>
+          <tr v-for="(rec, recIDX) in byDay" :key="String(rec.month)+rec.day">
+            <td :style='`
+              --start: ${calcCapitalPercent(byDay[recIDX - 1], "start")};
+              --size: ${calcCapitalPercent(rec, "end")}
+            `'> <span class="data"> ￥{{rec.capital}} </span> </td>
           </tr>
-          <tr>
-            <td style="--start: 0.0; --size: 0.0"> <span class="data"> ￥0 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.0; --size: 0.0"> <span class="data"> ￥0 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.0; --size: 0.0"> <span class="data"> ￥0 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.0; --size: 0.5"> <span class="data"> ￥50 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.5; --size: 0.5"> <span class="data"> ￥50 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.5; --size: 0.5"> <span class="data"> ￥50 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.5; --size: 0.7"> <span class="data"> ￥70 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.7; --size: 0.7"> <span class="data"> ￥70 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.7; --size: 0.7"> <span class="data"> ￥70 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.7; --size: 0.7"> <span class="data"> ￥70 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.7; --size: 0.7"> <span class="data"> ￥70 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.7; --size: 0.7"> <span class="data"> ￥70 </span> </td>
-          </tr>
-          <tr>
-            <td style="--start: 0.7; --size: 0.7"> <span class="data"> ￥70 </span> </td>
+        </tbody>
+      </table>
+
+      <table id="chart-money-count-all" class="charts-css line show-data-axes show-heading" style="--color: #f46565;">
+        <caption style="height: 3em;"> <b>迟到概览</b>（基金总计 / 基金日计 / 日统计详情） </caption>
+        <tbody>
+          <tr v-for="(rec, recIDX) in byDay" :key="String(rec.month)+rec.day">
+            <td :style='`
+              --start: ${calcCapitalAllPercent(byDay[recIDX - 1], "start")};
+              --size: ${calcCapitalAllPercent(rec, "end")}
+            `'> <span class="data"> ￥{{rec.capitalACC}} </span> </td>
           </tr>
         </tbody>
       </table>
@@ -87,101 +60,151 @@
 export default {
   data () {
     return {
-      byDay: [
-        {
-          month: 3,
-          day: 22,
-          weather: 'sun',
-          record: [
-            {
-              name: '方方'
-            },
-            {
-              name: '一光'
+      raw: require('./late').default
+    }
+  },
+  computed: {
+    // 进行基本的数据清洗
+    data () {
+      const basicWash = x => {
+        x.record = x.record || []
+        x.weather = x.weather || 'sun'
+        return x
+      }
+      return this.raw.map(basicWash)
+    },
+    // 按月维度划分数组
+    splitByMonth () {
+      return this.data.reduceRight((h, c) => {
+        let lastMonth = h[h.length - 1]
+        if (!lastMonth) {
+          lastMonth = []
+          h.push(lastMonth)
+        }
+        const lastDay = lastMonth[lastMonth.length - 1] || {}
+        if (lastDay.day !== 1) {
+          lastMonth.push(c)
+        } else {
+          h.push([c])
+        }
+        return h
+      }, []).map(x => x.reverse()).reverse()
+    },
+    // 计算迟到基金情况
+    withCapital () {
+      return this.splitByMonth.reduce((h, month) => {
+        const lateNameInMonthMap = {}
+        const monthWithCapital = month.reduce((newMonth, day) => {
+          const lastDay = newMonth[newMonth.length - 1]
+          const lastCapital = lastDay?.capital || 0
+
+          let dayCapital = day.record.reduce((h, rec) => {
+            // TODO 选择分享还是请两杯奶茶
+            const name = rec.name
+            const lastLateTime = lateNameInMonthMap[name]
+            const curLateTime = lateNameInMonthMap[name] = lastLateTime
+              ? lastLateTime + 1
+              : 1
+            switch (curLateTime) {
+              case 1:
+              case 2:
+                return h
+              case 3:
+                return h + 50
+              default:
+                return h + (curLateTime - 3) * 20
             }
-          ]
-        },
-        {
-          month: 3,
-          day: 23,
-          weather: 'cloud',
-        },
-        {
-          month: 3,
-          day: 24,
-          weather: 'rain',
-          record: [
-            {
-              name: '一光'
+          }, 0)
+
+          // 如果一周都没迟到记录则二军出￥50
+          // TODO fixme
+          if ((newMonth.length + 1) % 7 === 0) {
+            const last7Days = newMonth.slice(-6)
+            last7Days.push(day)
+            if (!last7Days.find(x => x.record.length > 0)) {
+              dayCapital = 50
             }
-          ]
-        },
-        {
-          month: 3,
-          day: 25,
-          weather: 'sun',
-        },
-        {
-          month: 3,
-          day: 26,
-          weather: 'cloud',
-          record: [
-            {
-              name: '一光'
-            }
-          ]
-        },
-        {
-          month: 3,
-          day: 27,
-          weather: 'sun',
-        },
-        {
-          month: 3,
-          day: 28,
-          weather: 'sun',
-        },
-        {
-          month: 3,
-          day: 29,
-          weather: 'rain',
-          record: [
-            {
-              name: '一光'
-            }
-          ]
-        },
-        {
-          month: 3,
-          day: 30,
-          weather: 'rain',
-        },
-        {
-          month: 3,
-          day: 31,
-          weather: 'rain',
-        },
-        {
-          month: 4,
-          day: 1,
-          weather: 'rain',
-        },
-        {
-          month: 4,
-          day: 2,
-          weather: 'cloud',
-        },
-        {
-          month: 4,
-          day: 3,
-          weather: 'sun',
-        },
-        {
-          month: 4,
-          day: 4,
-          weather: 'sun',
-        },
-      ]
+          }
+
+          const dayWithCapital = {
+            ...day,
+            dayCapital,
+            capital: lastCapital + dayCapital
+          }
+          newMonth.push(dayWithCapital)
+          return newMonth
+        }, [])
+
+        h.push(monthWithCapital)
+        return h
+      }, [])
+    },
+    withCapitalACC () {
+      const flattend = this.withCapital.flat(1)
+      return flattend.reduce((h, c) => {
+        const lastDay = h[h.length - 1]
+        const lastDayCapital = lastDay?.dayCapital || 0
+        const lastCapitalACC = lastDay?.capitalACC || 0
+        h.push({
+          ...c,
+          capitalACC: lastCapitalACC + lastDayCapital
+        })
+        return h
+      }, [])
+    },
+    byDay () {
+      return this.withCapitalACC
+    },
+    curMonth () {
+      return this.byDay.reduceRight((h, c) => {
+        const last = h[h.length - 1] || {}
+        if (last.day !== 1) {
+          h.push(c)
+        }
+        return h
+      }, []).reverse()
+    }
+  },
+  methods: {
+    offsetDay (dayOrIDX, offset) {
+      let idx
+      if (dayOrIDX instanceof Object) {
+        idx = this.byDay.findIndex(x => x === dayOrIDX)
+      }
+      return this.byDay[idx + offset] || null
+    },
+    lastDay (dayOrIDX) {
+      return this.offsetDay(dayOrIDX, -1)
+    },
+    nextDay (dayOrIDX) {
+      return this.offsetDay(dayOrIDX, +1)
+    },
+    calcCapitalPercent (day, type, options) {
+      const { base: baseVal, max: maxVal } = options || {}
+
+      const nextDay = this.nextDay(day)
+      if (nextDay?.day === 1 && type === 'start') {
+        return '0.0'
+      }
+
+      const max = maxVal || Math.floor(
+        Math.max.apply(null, this.byDay.map(x => x.capital)) * 
+        1.2
+      )
+      const base = baseVal || (day?.capital || 0)
+      if (!base) {
+        return '0.0'
+      } else {
+        return String((base / max).toFixed(1))
+      }
+    },
+    calcCapitalAllPercent (day) {
+      const max = Math.floor(
+        this.byDay[this.byDay.lenght - 1]?.capitalACC * 
+        1.5
+      )
+      const base = day?.capitalACC || 0
+      return this.calcCapitalPercent(day, 'all', { base, max })
     }
   }
 }
@@ -200,6 +223,7 @@ export default {
 }
 .charts-con {
   position: relative;
+  top: 100px;
   height: 250px;
   width: 100%;
   max-width: 1200px;
@@ -217,6 +241,9 @@ export default {
 .charts-con > table.column th {
   background-color: #f6f6f6;
 }
+/* .charts-con > table span.data {
+  text-indent: -2.7em;
+} */
 
 /************************* Chart */
 
@@ -224,8 +251,13 @@ export default {
   --labels-size: 4.8rem;
   --labels-gap: 0.5rem;
 }
-#chart-money-count {
-  top: -250px;
+#chart-money-count-day {
+  top: -100px;
+  height: 100px;
+}
+#chart-money-count-all {
+  top: -270px;
+  height: 170px;
 }
 
 .sun {
---
+++ blogs/.vuepress/components/Article/Bax/late.js
@@ -0,0 +1,123 @@
+export default [
+  {
+    month: 3,
+    day: 22,
+    record: [
+      {
+        "name": "方方"
+      },
+      {
+        "name": "一光"
+      }
+    ]
+  },
+  {
+    month: 3,
+    day: 23,
+    weather: "cloud"
+  },
+  {
+    month: 3,
+    day: 24,
+    weather: "rain",
+    record: [
+      {
+        "name": "一光"
+      }
+    ]
+  },
+  {
+    month: 3,
+    day: 25,
+  },
+  {
+    month: 3,
+    day: 26,
+    weather: "cloud",
+    record: [
+      {
+        "name": "一光"
+      }
+    ]
+  },
+  {
+    month: 3,
+    day: 27,
+  },
+  {
+    month: 3,
+    day: 28,
+  },
+  {
+    month: 3,
+    day: 29,
+    weather: "rain",
+    record: [
+      {
+        "name": "一光"
+      }
+    ]
+  },
+  {
+    month: 3,
+    day: 30,
+    weather: "rain"
+  },
+  {
+    month: 3,
+    day: 31,
+    weather: "rain"
+  },
+  {
+    month: 4,
+    day: 1,
+    weather: "rain"
+  },
+  {
+    month: 4,
+    day: 2,
+    weather: "cloud"
+  },
+  {
+    month: 4,
+    day: 3,
+  },
+  {
+    month: 4,
+    day: 4,
+  },
+  {
+    month: 4,
+    day: 5,
+    weather: "cloud"
+  },
+  {
+    month: 4,
+    day: 6,
+    weather: "cloud"
+  },
+  {
+    month: 4,
+    day: 7,
+    weather: "rain"
+  },
+  {
+    month: 4,
+    day: 8,
+    weather: "cloud"
+  },
+  {
+    month: 4,
+    day: 9,
+  },
+  {
+    month: 4,
+    day: 10,
+    weather: "cloud"
+  },
+  {
+    month: 4,
+    day: 11,
+    weather: "rain"
+  }
+]
