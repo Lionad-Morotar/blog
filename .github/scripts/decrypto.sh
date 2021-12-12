5e0d5089c912be7cb22a81b4b76da16ec3d6a290
仿生狮子 <tangnad@qq.com>
2021-12-12 21:48:10 +0800

    chore: 更改解密脚本格式

--- .github/scripts/decrypto.sh
+++ .github/scripts/decrypto.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 # Decrypt the file
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS" \
---output ./blogs/.vuepress/private/valine-id.js ./blogs/.vuepress/private/valine-id.js.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS" --output ./blogs/.vuepress/private/valine-id.js ./blogs/.vuepress/private/valine-id.js.gpg
 
 echo "Decrypt valine-id done"
 
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS" \
---output ./blogs/.vuepress/private/valine-key.js ./blogs/.vuepress/private/valine-key.js.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS" --output ./blogs/.vuepress/private/valine-key.js ./blogs/.vuepress/private/valine-key.js.gpg
 
 echo "Decrypt valine-key done"
