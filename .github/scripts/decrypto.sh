552457a43618d5d52f8a1ce8ab6f020c8e69a288
仿生狮子 <tangnad@qq.com>
2021-12-12 21:42:27 +0800

    chore: change gpg path

--- .github/scripts/decrypto.sh
+++ .github/scripts/decrypto.sh
@@ -2,11 +2,11 @@
 
 # Decrypt the file
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS" \
---output $GITHUB_WORKSPACE/blogs/.vuepress/private/valine-id.js $GITHUB_WORKSPACE/blogs/.vuepress/private/valine-id.js.gpg
+--output ./blogs/.vuepress/private/valine-id.js ./blogs/.vuepress/private/valine-id.js.gpg
 
 echo "Decrypt valine-id done"
 
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS" \
---output $GITHUB_WORKSPACE/blogs/.vuepress/private/valine-key.js $GITHUB_WORKSPACE/blogs/.vuepress/private/valine-key.js.gpg
+--output ./blogs/.vuepress/private/valine-key.js ./blogs/.vuepress/private/valine-key.js.gpg
 
 echo "Decrypt valine-key done"
