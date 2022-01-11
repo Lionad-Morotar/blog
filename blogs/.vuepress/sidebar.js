5ca7ec38e1c50c6cda32fd8f611b74177c3c6cb8
仿生狮子 <tangnad@qq.com>
2022-01-12 02:25:34 +0800

    fix: sidebar

--- blogs/.vuepress/sidebar.js
+++ blogs/.vuepress/sidebar.js
@@ -43,7 +43,6 @@ const sidebars = [
     path: '/flows/',
     children: [
       // 'flows/brain-history',
-      'flows/art',
       // 'flows/rss',
       'flows/books',
       'flows/punctuations',
