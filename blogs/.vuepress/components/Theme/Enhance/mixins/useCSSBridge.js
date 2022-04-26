3fc6aa33cd562a5b40aad84bfa7050213236bf72
仿生狮子 <tangnad@qq.com>
2022-04-26 14:30:09 +0800

    stash: use Idle callback

--- blogs/.vuepress/components/Theme/Enhance/mixins/useCSSBridge.js
+++ blogs/.vuepress/components/Theme/Enhance/mixins/useCSSBridge.js
@@ -1,26 +1,25 @@
-/** @file Crisp / 没有废除 Crisp 的原因是可以看到用户的实时浏览轨迹 */
-
 const utils = require('../utils')
 
 module.exports = {
     mounted() {
-        // TODO useIdleCallBack
         const listenType = utils.isMobile ? 'touchstart' : 'mousedown'
         document.addEventListener(listenType, function(event) {
-            const root = document.body
-            const target = event.target
-            const touch = event.touches ? event.touches[0] : event
-            const meter = 'px'
-
-            root.style.setProperty('--clientx', touch.clientX + meter)
-            root.style.setProperty('--clienty', touch.clientY + meter)
-            root.style.setProperty('--pagex', touch.pageX + meter)
-            root.style.setProperty('--pagey', touch.pageY + meter)
-
-            target.style.setProperty('--w', target.clientWidth + meter)
-            target.style.setProperty('--h', target.clientHeight + meter)
-            target.style.setProperty('--x', target.offsetLeft + meter)
-            target.style.setProperty('--y', target.offsetTop + meter)
+            utils.requestIdleCallback(() => {
+                const root = document.body
+                const target = event.target
+                const touch = event.touches ? event.touches[0] : event
+                const meter = 'px'
+    
+                root.style.setProperty('--clientx', touch.clientX + meter)
+                root.style.setProperty('--clienty', touch.clientY + meter)
+                root.style.setProperty('--pagex', touch.pageX + meter)
+                root.style.setProperty('--pagey', touch.pageY + meter)
+    
+                target.style.setProperty('--w', target.clientWidth + meter)
+                target.style.setProperty('--h', target.clientHeight + meter)
+                target.style.setProperty('--x', target.offsetLeft + meter)
+                target.style.setProperty('--y', target.offsetTop + meter)
+            })
         })
     }
 }
