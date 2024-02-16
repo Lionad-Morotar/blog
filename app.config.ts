81cf93b9df33515732115495317c19697dd7db9a
仿生狮子 <tangnad@qq.com>
2024-02-16 21:26:36 +0800

    text: fix spell

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default defineAppConfig({
     }
   },
   seo: {
-    siteName: 'Lionad\'s Blog and Knowledgebase',
+    siteName: 'Lionad\'s Blog and KnowledgeBase',
   },
   header: {
     logo: {
