264b1eebf18888d643c0a42eb1327c16c86e616d
仿生狮子 <tangnad@qq.com>
2024-03-31 14:15:03 +0800

    chore: add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,65 +1,111 @@
+import rssPosts from "./rss.js";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   vue: {
     compilerOptions: {
-      isCustomElement: tag => [].includes(tag)
+      isCustomElement: (tag) => [].includes(tag),
     },
   },
-  extends: ['@nuxt/ui-pro'],
+  extends: ["@nuxt/ui-pro"],
   modules: [
-    '@nuxt/content',
-    '@nuxt/ui',
-    '@nuxthq/studio',
-    '@nuxtjs/fontaine',
+    "@nuxt/content",
+    "@nuxt/ui",
+    "@nuxthq/studio",
+    "@nuxtjs/fontaine",
+    "@nuxtjs/sitemap",
+    // ! wierd error, disable for a while
+    // "@nuxtjs/feed",
+    // ! cant fetch twimoji error, so disable for a while
     // 'nuxt-og-image'
   ],
+  site: {
+    url: 'https://www.lionad.art',
+  },
+  // @ts-ignore see https://github.com/nuxt-community/feed-module
+  feed: {
+    path: "/rss.xml",
+    async create(feed) {
+      feed.options = {
+        title: "Lionad's blog",
+        link: "https://www.lionad.art/rss.xml",
+        description: "Feed for Lionad's Blog",
+      };
+      rssPosts.forEach((post) => {
+        feed.addItem({
+          title: post.title,
+          id: post.url,
+          link: post.url,
+          description: post.description,
+          content: post.content,
+        });
+      });
+      feed.addCategory("Lionad");
+      feed.addContributor({
+        name: "Lionad",
+        email: "1806234223@qq.com",
+        link: "https://www.lionad.art",
+      });
+    },
+  },
   hooks: {
     // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter((c) => ['Commend', 'Compare', 'ReadBase64', 'LLink', 'Spark', 'UButton', 'UIcon', 'AspectRatio'].includes(c.pascalName))
+    "components:extend": (components) => {
+      const globals = components.filter((c) =>
+        [
+          "Commend",
+          "Compare",
+          "ReadBase64",
+          "LLink",
+          "Spark",
+          "UButton",
+          "UIcon",
+          "AspectRatio",
+        ].includes(c.pascalName)
+      );
 
-      globals.forEach((c) => c.global = true)
-    }
+      globals.forEach((c) => (c.global = true));
+    },
   },
   ui: {
-    icons: ['heroicons', 'simple-icons']
+    icons: ["heroicons", "simple-icons"],
   },
   content: {
     highlight: {
       theme: {
-        default: 'github-light',
-        dark: 'github-dark',
-        sepia: 'monokai'
-      }
-    }
+        default: "github-light",
+        dark: "github-dark",
+        sepia: "monokai",
+      },
+    },
   },
   routeRules: {
-    '/api/search.json': { prerender: true },
+    "/api/search.json": { prerender: true },
   },
   devtools: {
     enabled: false,
   },
   typescript: {
-    strict: false
+    strict: false,
   },
   mdc: {
     highlight: {
       langs: [
-        'bash',
-        'cpp',
-        'css',
-        'glsl',
-        'html',
-        'less',
-        'js',
-        'powershell',
-        'scss',
-        'shell',
-        'ts',
-        'vue',
-        'makefile',
-        'csharp'
-      ]
-    }
-  }
-})
+        "bash",
+        "cpp",
+        "css",
+        "glsl",
+        "html",
+        "less",
+        "js",
+        "powershell",
+        "scss",
+        "shell",
+        "ts",
+        "vue",
+        "makefile",
+        "csharp",
+      ],
+    },
+  },
+});
---
+++ rss.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    title: '💻 本地部署 Qwen 翻译网页',
+    id: 'https://lionad.art/articles/local-translator/',
+    link: 'https://lionad.art/articles/local-translator/',
+    description: '沉浸式翻译太贵辽，试试本地部署 LLM 用来翻译网页的可行性。',
+    content: '直接说结论。相比 Google 或百度等机翻，AI 翻译在语句通顺程度、语气、名词保留等方面好得多。尽管测试下来（主观来看），开源 LLM 模型的翻译质量还差 GPT3.5 一截，而且也容易出稀奇古怪的问题，但实话说<del>（习惯就好）</del>已经够用了。',
+  }
+]
