f9a97b412ff9787fa1f29a8cc724fa538a054039
Lionad-Morotar <tangnad@qq.com>
2026-03-05 06:37:41 +0000

    Deploying to gh-pages from @ Lionad-Morotar/blog@77acc5e2eec95d632a26ecac682ffa7c8056ad54 🚀

---
+++ _nuxt/DF_3LlEt.js
@@ -0,0 +1,1 @@
+import{r as n}from"./B3Zouy09.js";import a from"./DPy1hcp4.js";import{G as s,o as t,s as e,v as i,y as p}from"./C5_tnI-t.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(t(),e(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
